5ed0aa346d52c6bd9b7edf608b9a5a848a3f6f08
Sonamkhurana <sonamranikhurana@gmail.com>
2020-12-04 18:23:59 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
 
-if (iOS) {
-  // Just use the link of your app on the AppStore and replace https:// with itms://
-  window.location.href = "https://apps.apple.com/fi/app/onni-ty%C3%B6aika/id1069896702?l=fi";
-}
